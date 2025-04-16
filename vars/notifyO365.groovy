c0eaa25acc8d0e65d493d215b275d3bf22e51157
kingxu <kingxu@midland.com.cn>
2025-04-16 11:46:47 +0800

    fix(notifyO365): 优化消息格式的可读性
    - 在项目名称、分支、Git消息和构建时间之间添加空行
    - 在消息末尾添加空行，以改善整体可读性

--- vars/notifyO365.groovy
+++ vars/notifyO365.groovy
@@ -6,10 +6,10 @@ def call(Map config = [:]) {
     def project = env.JOB_NAME ?: 'Unknown'
     def gitCommit = sh(script: "git log -1 --pretty=%B", returnStdout: true)?.trim() ?: 'Unknown'
     def buildTime = currentBuild.duration / 1000 ?: '-1'
-    def message = """****${project}****
-        **Branch**: ${branch}
-        **GIT MESSAGE**: ${gitCommit}
-        **Build Time**: ${buildTime} sec
+    def message = """****${project}****\n\n
+        **Branch**: ${branch}\n\n
+        **GIT MESSAGE**: ${gitCommit}\n\n
+        **Build Time**: ${buildTime} sec\n\n
         """
     def webhook = config.webhookUrl ?: WebhookUtil.getWebhookForBranch(branch)
 
