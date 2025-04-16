04061fc7dfc85576d6859d42356da3cb5b4f0852
kingxu <kingxu@midland.com.cn>
2025-04-16 11:44:07 +0800

    refactor(notifyO365): 重构通知模块
    - 移除了冗余的参数设置和判断逻辑
    - 使用 Jenkins 环境变量替代手动获取分支和项目名称
    - 优化了消息格式，删除了不必要的变量
    - 简化了代码结构，提高了可读性和维护性

--- vars/notifyO365.groovy
+++ vars/notifyO365.groovy
@@ -1,31 +1,21 @@
 import org.example.utils.WebhookUtil
 
 def call(Map config = [:]) {
-    // 如果未提供 gitCommit，则自动获取最新提交的 commit message
-    if (!config.gitCommit || config.gitCommit == 'unknown') {
-        // 注意：请确保此步骤在有 node 环境下执行
-        config.gitCommit = sh(script: "git log -1 --pretty=%B", returnStdout: true)?.trim()
-    }
-    // 如果未提供 buildTime，则尝试从 currentBuild 中计算（单位秒）
-    if ((config.buildTime == '-1' || !config.buildTime) && currentBuild?.duration) {
-        config.buildTime = currentBuild.duration / 1000
-    }
-    def status = config.status ?: 'Success'
-    def branch = config.branch ?: 'unknown'
-    def project = config.project ?: 'unknown'
-    def gitCommit = config.gitCommit ?: 'unknown'
-    def buildTime = config.buildTime ?: '-1'
-    def message = config.message ?: "Build for branch ${branch} has ${status == 'Success' ? 'succeeded' : 'failed'}."
-    def message2= "****${project}****\n\n" +
-                  "**Branch**: ${branch}\n\n" +
-                  "**GIT MESSAGE**: ${gitCommit}\n\n" +
-                  "**Build Time**: ${buildTime} sec\n\n"
-    // 如果没传 webhookUrl，就自动根据分支取
+    def status = currentBuild.currentResult ?: 'SUCCESS'
+    def branch = env.BRANCH_NAME ?: 'master'
+    def project = env.JOB_NAME ?: 'Unknown'
+    def gitCommit = sh(script: "git log -1 --pretty=%B", returnStdout: true)?.trim() ?: 'Unknown'
+    def buildTime = currentBuild.duration / 1000 ?: '-1'
+    def message = """****${project}****
+        **Branch**: ${branch}
+        **GIT MESSAGE**: ${gitCommit}
+        **Build Time**: ${buildTime} sec
+        """
     def webhook = config.webhookUrl ?: WebhookUtil.getWebhookForBranch(branch)
 
     office365ConnectorSend(
         webhookUrl: webhook,
-        message: message2,
+        message: message,
         status: status,
         adaptiveCards: true,
     )
